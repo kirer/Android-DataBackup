22beff87390edaa22c971746fa10f660533710cc
Kirer <lin77777777777777k@gmail.com>
2024-12-31 10:11:45 +0800

    1

--- source/core/service/src/main/kotlin/com/xayah/core/service/HttpService.kt
+++ source/core/service/src/main/kotlin/com/xayah/core/service/HttpService.kt
@@ -4,13 +4,16 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import com.google.gson.reflect.TypeToken
 import com.safframework.server.core.AndroidServer
 import com.safframework.server.core.http.Response
 import com.xayah.core.data.repository.AppsRepo
 import com.xayah.core.data.repository.ListDataRepo
 import com.xayah.core.database.dao.PackageDao
+import com.xayah.core.model.ContributorItem
 import com.xayah.core.model.OpType
 import com.xayah.core.util.DateUtil
+import com.xayah.core.util.GsonUtil
 import com.xayah.core.util.PathUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -21,23 +24,28 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
+data class BackupRequestParams(val package_name: String)
+
 @AndroidEntryPoint
 class HttpService : Service() {
 
     @Inject
-    private lateinit var backupServiceLocalImpl: com.xayah.core.service.packages.backup.ProcessingServiceProxyLocalImpl
+    lateinit var backupServiceLocalImpl: com.xayah.core.service.packages.backup.ProcessingServiceProxyLocalImpl
 
     @Inject
-    private lateinit var restoreServiceLocalImpl: com.xayah.core.service.packages.restore.ProcessingServiceProxyLocalImpl
+    lateinit var restoreServiceLocalImpl: com.xayah.core.service.packages.restore.ProcessingServiceProxyLocalImpl
 
     @Inject
-    private lateinit var appsRepo: AppsRepo
+    lateinit var appsRepo: AppsRepo
 
     @Inject
-    private lateinit var listDataRepo: ListDataRepo
+    lateinit var listDataRepo: ListDataRepo
 
     @Inject
-    private lateinit var appsDao: PackageDao
+    lateinit var appsDao: PackageDao
+
+    @Inject
+    lateinit var gsonUtil: GsonUtil
 
     private var androidServer: AndroidServer? = null
 
@@ -59,22 +67,38 @@ class HttpService : Service() {
                 get("/hello") { _, response: Response ->
                     response.setBodyText("hello world")
                 }
-                get("/backup") { request, response: Response ->
+                post("/backup") { request, response: Response ->
+                    Log.e("KKK", "backup")
                     runBlocking {
                         launch {
-                            listDataRepo.getAppList().map { appList ->
-                                appList.map {
-                                    val activated = it.packageName == request.params()["package_name"]
-                                    appsDao.activateById(it.id, activated)
+                            try {
+                                Log.e("KKK", "params: ${request.content()}")
+                                val params:Map<String, String> = gsonUtil.fromJson(
+                                    request.content(), object : TypeToken<Map<String, String>>() {}.type
+                                )
+                                val packageName = params["package_name"]
+                                Log.e("KKK", "packageName: $packageName")
+                                Log.e("KKK", "packageName: ${listDataRepo.getAppList()}")
+                                listDataRepo.getAppList().map { appList ->
+                                    appList.map {
+                                        Log.e("KKK", "app: ${it}")
+                                        val activated = it.packageName == packageName
+                                        appsDao.activateById(it.id, activated)
+                                        Log.e("KKK", "activated: ${it.packageName}")
+                                    }
                                 }
-                            }
-                            backupServiceLocalImpl.initialize()
-                            backupServiceLocalImpl.preprocessing()
-                            backupServiceLocalImpl.processing()
-                            backupServiceLocalImpl.postProcessing()
-                            backupServiceLocalImpl.destroyService()
-                            appsDao.queryActivated(OpType.BACKUP).map {
-                                appsRepo.protectApp("", it)
+                                backupServiceLocalImpl.initialize()
+                                backupServiceLocalImpl.preprocessing()
+                                backupServiceLocalImpl.processing()
+                                backupServiceLocalImpl.postProcessing()
+                                backupServiceLocalImpl.destroyService()
+                                appsDao.queryActivated(OpType.BACKUP).map {
+                                    appsRepo.protectApp("", it)
+                                    Log.e("KKK", "protectApp: ${it.packageName}")
+                                }
+                            }catch (e: Exception) {
+                                Log.e("KKK", "exception > $e")
+                                e.printStackTrace()
                             }
                         }.join()
                         response.setBodyText("Backup started")
